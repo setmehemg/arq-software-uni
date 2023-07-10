8b4435b61189c8d3a606e37624196759e80c85df
setmehemg <henrique.gewehr@universo.univates.br>
2023-07-10 20:52:19 +0000

    modified:   update-homologacao.sh

--- update-homologacao.sh
+++ update-homologacao.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker pull henriquemg/gcs-laravel:latest
-docker compose -f docker-compose.stage.yml -p stage up -d
-docker exec eventos-homolog php artisan migrate
+sudo docker pull -q henriquemg/gcs-laravel:latest
+sudo docker compose -f docker-compose.stage.yml -p stage up -d
+sudo docker exec eventos-homolog php artisan migrate
