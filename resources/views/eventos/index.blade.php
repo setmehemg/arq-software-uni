53cd0f7b5f3af7db19f103c054b12198baadc75f
setmehemg <henrique.gewehr@universo.univates.br>
2023-07-11 22:08:21 +0000

    new file:   database/migrations/2023_07_11_220428_tipo_x_create_table.php 	modified:   resources/views/eventos/index.blade.php

--- resources/views/eventos/index.blade.php
+++ resources/views/eventos/index.blade.php
@@ -9,7 +9,7 @@
         <div class="row">
             <div class="col-lg-12 margin-tb">
                 <div class="pull-left">
-                    <h2>Eventos</h2>
+                    <h2>Eventos - Alterado</h2>
                 </div>
             </div>
         </div>
